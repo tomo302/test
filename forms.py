af24748daa00722519930a004e37ed07226f184b
tomo302 <aptget.gg@gmail.com>
2018-03-09 14:51:50 +0900

    テスト終了削除

--- forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from crud.models import Member
-
-
-class MemberForm(ModelForm):
-    class Meta:
-        model = Member
-        fields = ('name','email','age',)
