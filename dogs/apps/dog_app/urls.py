05d153ee4929479e6afbea74bb505614e84210b9
tomo302 <aptget.gg@gmail.com>
2018-03-14 23:45:08 +0900

    editまで

--- dogs/apps/dog_app/urls.py
+++ dogs/apps/dog_app/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.index, name='index'),
+    url(r'^$',views.index),
+    url(r'^create$', views.create),
+    url(r'^edit/(?P<id>\d+)/$', views.edit),
+    url(r'^update/(?P<id>\d+)/$', views.update),
     
 
 ]
--- dogs/apps/dog_app/views.py
+++ dogs/apps/dog_app/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Dog
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'dog_app/index.html', {})
+    dogs = Dog.objects.all()
+    context = {'dogs':dogs}
+    return render(request, 'dog_app/index.html', context)
+
+
+def create(request):
+
+    print(request.POST)
+    dog = Dog(name=request.POST['name'], breed=request.POST['breed'])
+
+    dog.save()
+
+    return redirect('/')
+
+
+def edit(request, id):
+    dog = Dog.objects.get(id=id)
+
+    context = {'dog':dog}
+    return render(request, 'dog_app/edit.html', context)
+
+
+def update(request, id):
+    dog = Dog.objects.get(id=id)
+    dog.name = request.POST['name']
+    dog.breed = request.POST['breed']
+    print(dog.name)
+    dog.save()
+    return redirect('/')
